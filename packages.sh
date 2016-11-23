000567359368bcb5b8aae17562137b0588edec9d
Rod Hall <rod.hall@comparethemarket.com>
2016-11-23 16:52:55 +0000

    Add packages.sh

---
+++ packages.sh
@@ -0,0 +1,14 @@
+apm install seti-ui &&
+apm install monokai-seti &&
+apm install atom-beautify &&
+apm install auto-detect-indentation &&
+apm install autoclose-html &&
+apm install highlight-selected &&
+apm install linter &&
+apm install linter-eslint &&
+apm install minimap &&
+apm install minimap-highlight-selected &&
+apm install open-recent &&
+apm install pigments &&
+apm install todo-show &&
+apm install language-cshtml
