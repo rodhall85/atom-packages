7dca8d98f30c845fa01a345af58ba4f32946f7f7
rodhall85 <rodhall@hotmail.co.uk>
2017-07-20 21:55:23 +0100

    Add pretty-json

--- packages.sh
+++ packages.sh
@@ -9,10 +9,10 @@ apm install linter-eslint &&
 apm install minimap &&
 apm install minimap-highlight-selected &&
 apm install open-recent &&
-apm install pigments &&
 apm install todo-show &&
 apm install language-cshtml &&
 apm install atom-typescript &&
 apm install last-cursor-position &&
 apm install language-terraform &&
-apm install split-diff
+apm install split-diff &&
+apm install pretty-json
